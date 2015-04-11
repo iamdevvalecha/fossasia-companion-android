83234be3be7021feae80fce00ed1d96d669abfbb
Abhishek Batra <abhishekbatra1993@gmail.com>
2015-04-11 17:35:35 +0530

    Replaced Map image with Google Map

--- app/src/main/java/org/fossasia/fragments/MapFragment.java
+++ app/src/main/java/org/fossasia/fragments/MapFragment.java
@@ -3,23 +3,27 @@ package org.fossasia.fragments;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.fossasia.R;
 
 import java.util.Locale;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends SupportMapFragment {
 
     private static final double DESTINATION_LATITUDE = 1.29677;
     private static final double DESTINATION_LONGITUDE = 103.786914;
     private static final String DESTINATION_NAME = "Plug-In@Blk71";
+    private GoogleMap mMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,9 +32,19 @@ public class MapFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mMap = getMap();
+        if(mMap != null) {
+            mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        }
     }
+
+    //    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//
+//        return inflater.inflate(R.layout.fragment_map, container, false);
+//    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
